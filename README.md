e0d93f71a8df8d485d91a6cb91596407246721b7
VictorAFBZhao <victor.alexanderzhao@gmail.com>
2025-04-15 23:56:10 -0300

    Ajustando o projeto

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 # Primeiro Commit
 ## Olá mundo
-=======
 # DW_dbt_sql_st
->>>>>>> a1c90d000f0cf2893eecfcd733ea05eeb2b3e8f6
---
+++ src/extract_load.py
@@ -0,0 +1,9 @@
+# Import
+50 min
+# Import das variaveis de ambiente
+
+# pegar a cotacao dos ativos
+
+# concatenar os meus ativos
+
+# salvar no banco de dados
